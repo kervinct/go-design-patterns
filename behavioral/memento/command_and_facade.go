2c3fd6c654f787f5a7ba6f7e28286a916730bb28
Zvnlanx <kechaotai@hotmail.com>
2020-11-11 21:35:40 +0800

    update memento

--- behavioral/memento/command_and_facade.go
+++ behavioral/memento/command_and_facade.go
@@ -2,46 +2,58 @@ package behavioral
 
 import "fmt"
 
+// Command 接口
 type Command interface {
 	GetValue() interface{}
 }
 
+// Volume 实现接口
 type Volume byte
 
+// GetValue 接口
 func (v Volume) GetValue() interface{} {
 	return v
 }
 
+// Mute 实现接口
 type Mute bool
 
+// GetValue 接口
 func (m Mute) GetValue() interface{} {
 	return m
 }
 
+// Memento 抽象包装结构
 type Memento struct {
 	memento Command
 }
 
+// Originator 备忘结构
 type Originator struct {
 	Command Command
 }
 
+// NewMemento 创建备忘
 func (o *Originator) NewMemento() Memento {
 	return Memento{memento: o.Command}
 }
 
+// ExtractAndStoreCommand 存储备忘
 func (o *Originator) ExtractAndStoreCommand(m Memento) {
 	o.Command = m.memento
 }
 
+// CareTaker 备忘列表
 type CareTaker struct {
 	mementoList []Memento
 }
 
+// Add 添加备忘
 func (c *CareTaker) Add(m Memento) {
 	c.mementoList = append(c.mementoList, m)
 }
 
+// Pop 取出备忘
 func (c *CareTaker) Pop() Memento {
 	if len(c.mementoList) > 0 {
 		tempMemento := c.mementoList[len(c.mementoList)-1]
@@ -52,6 +64,7 @@ func (c *CareTaker) Pop() Memento {
 	return Memento{}
 }
 
+// Memento 获取指定备忘
 func (c *CareTaker) Memento(i int) (Memento, error) {
 	if len(c.mementoList) < i || i < 0 {
 		return Memento{}, fmt.Errorf("Index not found")
@@ -59,16 +72,19 @@ func (c *CareTaker) Memento(i int) (Memento, error) {
 	return c.mementoList[i], nil
 }
 
+// MementoFacade 备忘外观
 type MementoFacade struct {
 	originator Originator
 	careTaker  CareTaker
 }
 
+// SaveSettings 备忘存储
 func (m *MementoFacade) SaveSettings(s Command) {
 	m.originator.Command = s
 	m.careTaker.Add(m.originator.NewMemento())
 }
 
+// RestoreSettings 获取备忘
 func (m *MementoFacade) RestoreSettings(i int) Command {
 	memento, _ := m.careTaker.Memento(i)
 	m.originator.ExtractAndStoreCommand(memento)
